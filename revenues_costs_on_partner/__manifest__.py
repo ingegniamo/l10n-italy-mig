e6e28e46b164af16e89cca764605b131442f6054
Francesco Pranzo <pranzo.f@stesi.eu>
2023-09-06 15:45:12 +0200

    migrato modulo revenues_costs_on_partner

---
+++ revenues_costs_on_partner/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Revenues and Costs on partner',
+    'author': 'STeSI Srl',
+    'license': 'OPL-1',
+    'cathegory': 'Accounting',
+    'website': 'https://www.stesi.srl',
+    'version': '16.0.1',
+    'depends': [
+        'contacts',
+        'account'
+    ],
+    'data': [
+        'views/res_partner.xml',
+        # 'views/account_type.xml',
+    ],
+
+}
+
---
+++ revenues_costs_on_partner/models/res_partner.py
@@ -0,0 +1,27 @@
+from odoo import models, fields, api
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    def _domain_account_costs(self):
+        # domain = []
+        # cost_account_type = self.env.ref('account.data_account_type_expenses')
+        # if cost_account_type:
+        domain = [('account_type', 'in', ['expense'])]
+        # else:
+        # domain = [('internal_type', '=', 'other')]
+        return domain
+
+    def _domain_account_revenues(self):
+        # domain = []
+        # revenues_account_type = self.env.ref('account.data_account_type_revenue')
+        # if revenues_account_type:
+        domain = [('account_type', 'in', ['income', 'income_other'])]
+        # else:
+        # domain = [('internal_type', '=', 'other')]
+        return domain
+
+    costs_account_id = fields.Many2one('account.account', string="Costs Account", domain=_domain_account_costs)
+    revenues_account_id = fields.Many2one('account.account', string="Revenues Account", domain=_domain_account_revenues)
+
