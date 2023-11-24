c2448dae0fdb332229d8c6f546a1a8fab761d55f
micheledic <dicroce.m@stesi.eu>
2023-11-24 09:38:06 +0100

    mig DDT

--- l10n_it_delivery_note_base/hooks.py
+++ l10n_it_delivery_note_base/hooks.py
@@ -2,12 +2,11 @@
 from odoo import SUPERUSER_ID, api
 
 
-def post_init_hook(cr, sequence):
+def post_init_hook(env):
     """
     Create DN types and their sequences after installing the module
     if they're not already exist
     """
-    env = api.Environment(cr, SUPERUSER_ID, {})
     companies = env["res.company"].search([])
     for company in companies:
         env["stock.delivery.note.type"].create_dn_types(company)
