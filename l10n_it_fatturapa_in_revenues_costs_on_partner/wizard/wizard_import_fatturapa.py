d5a1231006484e68364c3f3501c891ff69367625
micheledic <dicroce.m@stesi.eu>
2024-02-06 17:46:37 +0100

    fix

--- l10n_it_fatturapa_in_revenues_costs_on_partner/wizard/wizard_import_fatturapa.py
+++ l10n_it_fatturapa_in_revenues_costs_on_partner/wizard/wizard_import_fatturapa.py
@@ -17,9 +17,31 @@ _logger = logging.getLogger(__name__)
 class WizardImportFatturapa(models.TransientModel):
     _inherit = "wizard.import.fatturapa"
     _description = "Import E-bill"
+    # def set_invoice_line_ids(
+    #     self, FatturaBody, credit_account_id, partner, wt_founds, invoice
+    # ):
+    #     res = super(WizardImportFatturapa,self).set_invoice_line_ids(FatturaBody, credit_account_id, partner, wt_founds, invoice)
+    #     if invoice.move_type in ['out_invoice'] and invoice.partner_id.revenues_account_id:
+    #         credit_account_id = invoice.partner_id.revenues_account_id.id
+    #     elif invoice.move_type in ['in_invoice'] and invoice.partner_id.costs_account_id:
+    #         credit_account_id = invoice.partner_id.costs_account_id.id 
+        
+    #     return res
     def invoiceCreate(self, fatt, fatturapa_attachment, FatturaBody, partner_id):
-         res = super().invoiceCreate(fatt, fatturapa_attachment, FatturaBody, partner_id)
-         res.invoice_line_ids._compute_account_id()
-         return res
+        res = super().invoiceCreate(fatt, fatturapa_attachment, FatturaBody, partner_id)
+        move_lines_product = res.invoice_line_ids.filtered(lambda f: f.display_type in ['product'])
+        account_id = False
+        if res.move_type in ['out_invoice'] and res.partner_id.revenues_account_id:
+            account_id =  res.partner_id.revenues_account_id.id
+        elif res.move_type in ['in_invoice'] and res.partner_id.costs_account_id:
+            account_id = res.partner_id.costs_account_id.id
+        if account_id:
+            move_lines_product.write({'account_id': account_id})
+        #  for move_line in res.invoice_line_ids.filtered(lambda f: f.display_type in ['product']):
+        #     if move_line.move_id.move_type in ['out_invoice'] and move_line.partner_id.revenues_account_id:
+        #         move_line.account_id = move_line.partner_id.revenues_account_id
+        #     elif move_line.move_id.move_type in ['in_invoice'] and move_line.partner_id.costs_account_id:
+        #         move_line.account_id = move_line.partner_id.costs_account_id
+        return res
 
  
