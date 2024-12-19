788b9f2dac252f089bf6ba31f704205a985f8feb
micheledic <dicroce.m@stesi.eu>
2024-12-19 10:37:11 +0100

    fix

--- revenues_costs_on_partner/models/account_type.py
+++ revenues_costs_on_partner/models/account_type.py
@@ -5,18 +5,11 @@ class AccountAccount(models.Model):
     _inherit = 'account.account'
 
     def _default_cost_type(self):
-        cost_type = self.env.ref('account.data_account_type_expenses')
-        if self.user_type_id == cost_type:
-            return True
-        else:
-            return False
+            return self.account_type == 'expense'
 
     def _default_revenue_type(self):
-        revenue = self.env.ref('account.data_account_type_revenue')
-        if self.user_type_id == revenue:
-            return True
-        else:
-            return False
+        
+            return self.account_type == 'income'
 
     cost = fields.Boolean(string="Cost", default=_default_cost_type)
     revenue = fields.Boolean(string="Revenue", default=_default_revenue_type)
