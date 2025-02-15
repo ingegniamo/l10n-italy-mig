8e0e86f30a30cf2ec2f5ac6982a2253b13e5a35e
Francesco Moccia <moccia.f@stesi.eu>
2025-02-15 11:20:21 +0100

    Update __manifest__.py

--- l10n_it_asset_management/__manifest__.py
+++ l10n_it_asset_management/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "ITA - Gestione Cespiti",
-    "version": "17.0.1.4.1",
+    "version": "18.0.0.0.1",
     "category": "Localization/Italy",
     "summary": "Gestione Cespiti",
     "author": "Openforce, Odoo Community Association (OCA)",
