31c8b064e36595999ed1ab97e64f101f23afa344
eLBati <elbaddy@gmail.com>
2025-04-18 12:21:22 +0200

    IMP l10n_it_account: _compute_totals_tax non viene più chiamato sulle imposte figlie

--- l10n_it_account/models/account_tax.py
+++ l10n_it_account/models/account_tax.py
@@ -161,43 +161,19 @@ class AccountTax(models.Model):
 
         tax = self.env["account.tax"].with_context(**context).browse(self.id)
         tax_name = tax._get_tax_name()
-        if not tax.children_tax_ids:
-            base_balance = tax.base_balance
-            balance = tax.balance
-            deductible_balance = tax.deductible_balance
-            undeductible_balance = tax.undeductible_balance
-            if registry_type == "supplier":
-                base_balance = -base_balance
-                balance = -balance
-                deductible_balance = -deductible_balance
-                undeductible_balance = -undeductible_balance
-            return (
-                tax_name,
-                base_balance,
-                balance,
-                deductible_balance,
-                undeductible_balance,
-            )
-        else:
-            # TODO remove?
-            base_balance = tax.base_balance
-            tax_balance = 0
-            deductible = 0
-            undeductible = 0
-            for child in tax.children_tax_ids:
-                child_balance = child.balance
-                tax_balance += child_balance
-                account_ids = (
-                    child.mapped("invoice_repartition_line_ids.account_id")
-                    | child.mapped("refund_repartition_line_ids.account_id")
-                ).ids
-                if account_ids:
-                    deductible += child_balance
-                else:
-                    undeductible += child_balance
-            if registry_type == "supplier":
-                base_balance = -base_balance
-                tax_balance = -tax_balance
-                deductible = -deductible
-                undeductible = -undeductible
-            return (tax_name, base_balance, tax_balance, deductible, undeductible)
+        base_balance = tax.base_balance
+        balance = tax.balance
+        deductible_balance = tax.deductible_balance
+        undeductible_balance = tax.undeductible_balance
+        if registry_type == "supplier":
+            base_balance = -base_balance
+            balance = -balance
+            deductible_balance = -deductible_balance
+            undeductible_balance = -undeductible_balance
+        return (
+            tax_name,
+            base_balance,
+            balance,
+            deductible_balance,
+            undeductible_balance,
+        )
