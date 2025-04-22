f7f95f0767e1b017c1abb54648b8539187ad06a7
micheledic <dicroce.m@stesi.eu>
2025-04-22 12:48:37 +0200

    up

--- l10n_it_asset_management/__manifest__.py
+++ l10n_it_asset_management/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "ITA - Gestione Cespiti",
-    "version": "18.0.0.0.3",
+    "version": "18.0.0.0.4",
     "category": "Localization/Italy",
     "summary": "Gestione Cespiti",
     "author": "Openforce, Odoo Community Association (OCA)",
