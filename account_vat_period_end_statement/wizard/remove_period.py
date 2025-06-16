6016a6eab545643699369606f42b5ab118518fb8
Arcadio Pinto <pinto.a@stesi.eu>
2025-06-16 17:40:26 +0200

    agg

--- account_vat_period_end_statement/wizard/remove_period.py
+++ account_vat_period_end_statement/wizard/remove_period.py
@@ -9,25 +9,14 @@ from odoo.tools.translate import _
 
 
 class RemovePeriod(models.TransientModel):
-    def _get_period_ids(self):
-        statement_model = self.env["account.vat.period.end.statement"]
-        res = []
-        if "active_id" in self.env.context:
-            statement = statement_model.browse(self.env.context["active_id"])
-            for period in statement.date_range_ids:
-                res.append((period.id, period.name))
-        return res
-
     _name = "remove.period.from.vat.statement"
     _description = "Remove period from VAT Statement"
 
-    period_id = fields.Selection(_get_period_ids, "Period", required=True)
+    period_id = fields.Many2one("date.range", "Period", required=True)
 
     def remove_period(self):
         self.ensure_one()
-        if "active_id" not in self.env.context:
-            raise UserError(_("Current statement not found"))
-        period = self.env["date.range"].browse(int(self.period_id))
+        period = self.env["date.range"].browse(int(self.period_id.id))
         period.vat_statement_id = False
         statement = self.env["account.vat.period.end.statement"].browse(
             self.env.context["active_id"]
