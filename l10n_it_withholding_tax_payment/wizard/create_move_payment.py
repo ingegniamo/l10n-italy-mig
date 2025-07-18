0c4fa3d7466d2e5a57e14f9c0b0039061135d5a1
micheledic <dicroce.m@stesi.eu>
2025-07-18 15:35:15 +0200

    fix

--- l10n_it_withholding_tax_payment/wizard/create_move_payment.py
+++ l10n_it_withholding_tax_payment/wizard/create_move_payment.py
@@ -28,7 +28,7 @@ class WizardWtMovePaymentCreate(models.TransientModel):
         view = self.env["ir.model.data"]._xmlid_lookup(
             "l10n_it_withholding_tax_payment.view_withholding_move_payment_form"
         )
-        view_id = view[2] or False
+        view_id = view[1] or False
 
         return {
             "name": _("Withholding Tax Payment"),
