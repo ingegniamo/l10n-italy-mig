922c1b773e7e9d42968e679374fd35dd964890d7
caracciolo-b-stesi <caracciolo.b@stesi.consulting>
2025-11-27 10:15:46 +0100

    Fix module l10n_it_financial_report_merge v. 19.0

--- l10n_it_withholding_tax/readme/ROADMAP.md
+++
@@ -1,2 +0,0 @@
-The Residual Net To Pay set in the Payment wizard should be converted in
-the wizard's currency when it changes
--- stesi_l10n_it_posfiscalcode_fix/static/src/js/model.js
+++
@@ -1,7 +0,0 @@
-odoo.define('stesi_l10n_it_posfiscalcode_fix.models', function(require){
-    "use strict";
-
-    var models = require('point_of_sale.models');
-
-    models.load_fields('res.partner', ['fiscalcode'])
-});
