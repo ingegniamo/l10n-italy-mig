79592d1741136214a7bcaaca5c3fe7001ce39878
Borruso <gborruso@dinamicheaziendali.it>
2026-01-02 17:28:45 +0100

    [MIG] l10n_it_sdd_cbi: Migration to 18.0

--- l10n_it_sdd_cbi/models/account_payment_line.py
+++ l10n_it_sdd_cbi/models/account_payment_line.py
@@ -7,9 +7,21 @@ from odoo import fields, models
 class AccountPaymentLineInherit(models.Model):
     _inherit = "account.payment.line"
 
+    def draft2open_payment_line_check(self):
+        res = super().draft2open_payment_line_check()
+        sepa_dd_lines = self.filtered(
+            lambda line: line.order_id.payment_method_id.code.startswith(
+                "cbi_sdd_italy"
+            )
+        )
+        sepa_dd_lines._check_sepa_direct_debit_ready()
+        return res
+
     def _prepare_account_payment_vals(self):
         vals = super()._prepare_account_payment_vals()
-        if self.order_id.payment_method_id.code.startswith("cbi_sdd_italy"):
+        payment_mode = self.order_id.payment_mode_id
+
+        if payment_mode.payment_method_id.code.startswith("cbi_sdd_italy"):
             today = fields.Date.context_today(self)
 
             if self.order_id.date_prefered == "due":
@@ -17,6 +29,6 @@ class AccountPaymentLineInherit(models.Model):
             elif self.order_id.date_prefered == "fixed":
                 requested_date = self.order_id.date_scheduled or today
             else:
-                requested_date = fields.Date.context_today(self)
+                requested_date = today
             vals["date"] = requested_date
         return vals
