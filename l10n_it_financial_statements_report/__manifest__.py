6783dce360ab1362ad9dc73d16e8901d9015a061
giovannigd <giovannidamato98@gmail.com>
2026-04-27 11:42:22 +0200

    [FIX] wizard_name error

--- l10n_it_financial_statements_report/__manifest__.py
+++ l10n_it_financial_statements_report/__manifest__.py
@@ -8,7 +8,7 @@
     "name": "ITA - Stato patrimoniale e conto economico",
     "summary": "Rendicontazione .pdf e .xls per stato patrimoniale e conto"
     " economico a sezioni contrapposte",
-    "version": "17.0.1.0.3",
+    "version": "17.0.1.0.4",
     "category": "Localization/Italy",
     "author": "Odoo Community Association (OCA), Openforce",
     "website": "https://github.com/OCA/l10n-italy"
